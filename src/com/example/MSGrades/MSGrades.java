3be40c7ba6c95d9703620f201f475de709e431ff
afreen <afreen.leo502@gmail.com>
2021-06-05 12:15:41 -0500

    Array loop string assignment

--- src/com/example/MSGrades/MSGrades.java
+++ src/com/example/MSGrades/MSGrades.java
@@ -1,34 +1,49 @@
 package com.example.MSGrades;
+import java.util.ArrayList;
+
 // import the java util ArrayList class module
+
 
 public class MSGrades {
 
     private final String[] teachers = {"Ms. Smith", "Mr. Johnson", "Mrs. Doe"};
-    
+
     // Create 4 ArrayLists below all private constant properties (final)
 
         // 1. a new instance of a students ArrayList storing strings
+       private final ArrayList<String> students =new ArrayList<>();
 
         // 2. a new instance of a math grades ArrayList storing integers
+       private final ArrayList<Integer> mathGrades = new ArrayList<Integer>();
         // 3. a new instance of a science grades ArrayList storing integers
-        // 4. a new instance of a math grades ArrayList storing integers
-
+        private final ArrayList<Integer> scienceGrades = new ArrayList<>();
+        // 4. a new instance of a english grades ArrayList storing integers
+        private final ArrayList<Integer> englishGrades = new ArrayList<>();
 
     public MSGrades(){
         // Add some students and their grades to the ArrayLists
 
             // add two students to the students ArrayList
-
+              students.add("John");
+              students.add("Mary");
             // add two corresponding values for the two students to the math grades ArrayList
-
+               mathGrades.add(69);
+               mathGrades.add(45);
             // add two corresponding values for the two students to the science grades ArrayList
-
-            // add two corresponding values for the two students to the english grades ArrayList
+             scienceGrades.add(78);
+             scienceGrades.add(98);
+        // add two corresponding values for the two students to the english grades ArrayList
+               englishGrades.add(100);
+               englishGrades.add(56);
     }
 
     public void printStudents(){
         // loop thru the students ArrayList with a For Each loop
             // print each students name on a new line as output
+        for(String student:students){
+            System.out.println("Student: "+student);
+        }
+
     }
 
     // Make a method that prints an individual students grades
@@ -41,26 +56,42 @@ public class MSGrades {
             // from that ArrayList of grades by index
         // Do this for the next two subjects
         // Total of four output statements
+     public void printStudentGrades(int studentIndex) {
 
-    public static void main(String[] args) {
+            System.out.println("Student:" +students.get(studentIndex));
+            System.out.println("Math: "+mathGrades.get(studentIndex));
+            System.out.println("Science: "+scienceGrades.get(studentIndex));
+            System.out.println("English: "+englishGrades.get(studentIndex));
+
+        }
+
+        public static void main(String[] args) {
 
         // create a new instance of MSGrades class and store in a variable
             // (anything like msGrade8)
         // look up how to print an empty new line
             // for readability
+        MSGrades msGrades= new MSGrades();
         
         System.out.println("Middle School Roster");
         System.out.println("====================");
 
         // invoke or run the printStudents method on our class instance
+         msGrades.printStudents();
 
         // Print another empty line if desired for readability
+        System.out.println("\n");
 
         // Use a for loop to loop thru the given teachers array on the class instance
         // inside the loop
             // create a new variable that stores the result of "Teacher: " +
                 // teachers name from the array using the counter variable
             // Print the new variable as output
+        for (int teacherIndex=0; teacherIndex< msGrades.teachers.length ;teacherIndex++) {
+             String teacherResult =msGrades.teachers[teacherIndex];
+            System.out.println("Teacher:" +teacherResult);
+            System.out.println("");
+        }
 
         System.out.println("\nStudent Grades:\n");
 
@@ -69,6 +100,14 @@ public class MSGrades {
             // pass the counter var as an index to the printStudentGrades method
             // to print each students info
             // Print a new line if you want it to look presentable
+        for (int studentIndex=0; studentIndex<msGrades.students.size() ; studentIndex++) {
+           msGrades.printStudentGrades(studentIndex);
+            System.out.println("");
+            }
+
+            System.out.println();
+
+        }
 
     }
-}
+
